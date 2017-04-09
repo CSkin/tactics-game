d0e1eaa2c52b00c4e3b3feb874ac05e3dad4146f
CSkin <colinskinner4@gmail.com>
2017-04-08 22:23:39 -0500

    Changed unit stats

--- levels/level0.js
+++ levels/level0.js
@@ -41,13 +41,14 @@ function Space(posY, posX, terrain) {
   this.inRange = null;
 }
 
-function Unit(id, sprite, faction, name, hp, damage, range, movement, posY, posX) {
+function Unit(id, sprite, faction, name, health, attack, defense, range, movement, posY, posX) {
   this.id = id;
   this.sprite = 'sprites/' + sprite;
   this.faction = faction;
   this.name = name;
-  this.hp = hp;
-  this.damage = damage;
+  this.health = health;
+  this.attack = attack;
+  this.defense = defense;
   this.range = range;
   this.movement = movement;
   this.moves = movement;
@@ -57,7 +58,7 @@ function Unit(id, sprite, faction, name, hp, damage, range, movement, posY, posX
   this.posX = posX;
 }
 
-var player0 = new Unit('player0', 'player.png', 'Player', 'Player Unit', 10, 1, 1, 5, 9, 4),
-    enemy0  = new Unit('enemy0', 'enemy.png', 'Enemy', 'Enemy Unit', 10, 1, 1, 5, 6, 11);
+var player0 = new Unit('player0', 'player.png', 'Player', 'Player Unit', 'Healthy', 1, 1, 1, 5, 9, 4),
+    enemy0  = new Unit('enemy0', 'enemy.png', 'Enemy', 'Enemy Unit', 'Healthy', 1, 1, 1, 5, 6, 11);
     
 var unitPlan = [ player0, enemy0 ];
