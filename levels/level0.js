e3dc9fc6c67e609d98719d69704d457b4e171d43
Colin Skinner <colinskinner4@gmail.com>
2017-04-17 23:28:11 -0500

    Update level0.js

--- levels/level0.js
+++ levels/level0.js
@@ -53,14 +53,14 @@ function Unit(id, sprite, faction, name, offense, defense, range, movement, posY
   this.range = range;
   this.movement = movement;
   this.moves = this.movement;
-  this.action = null;
+  this.moving = null;
   this.path = null;
   this.attacks = 1;
   this.posY = posY;
   this.posX = posX;
 }
 
-var player0 = new Unit('player0', 'player.png', 'Player', 'Player Unit', 2, 2, 1, 5, 9, 4),
+var player0 = new Unit('player0', 'player.png', 'Player', 'Player Unit', 2, 2, 1, 10, 9, 4),
     enemy0  = new Unit('enemy0', 'enemy.png', 'Enemy', 'Enemy Unit', 2, 1, 1, 5, 6, 11);
     
 var unitPlan = [ player0, enemy0 ];
